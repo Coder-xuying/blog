8c9afd1cd14fe5331c88855ef72625ec5e6a0f4d
xy <moon.xu@temu.com>
2024-05-14 23:03:01 +0800

    Update

---
+++ test.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+
+from fsevents import Observer, Stream
+import subprocess
+
+def on_save(event):
+    print(f"File saved: {event.name}")
+    script_path = "/Users/xuying/Desktop/个人/github_pro/blog/auto_commit.sh"  # 外部脚本或程序的路径
+    subprocess.run([script_path])
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python monitor_directory.py /path/to/directory")
+        sys.exit(1)
+
+    directory_path = sys.argv[1]
+
+    observer = Observer()
+    stream = Stream(on_save, directory_path, file_events=True)
+    observer.schedule(stream)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+if __name__ == "__main__":
+    main()
+
